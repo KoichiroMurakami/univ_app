e267c7cf2bf6c71910ebe44252807a2eb3e4538f
Rick516 <k.murakami516@gmail.com>
2020-07-05 18:51:10 +0900

    students new action added

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,24 @@ class StudentsController < ApplicationController
     def index
         @students  = Student.all
     end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.new(students_params)
+        if @student.save
+            flash[:success] = "you ahave successfully signed up"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    
+    def students_params
+        params.require(:student).permit(:name, :email)
+    end
 end
